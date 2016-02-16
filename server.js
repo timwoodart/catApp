30bf692894afd3f52442829ab394744de9569a47
Timothy Wood <timwoodwood@gmail.com>
2016-02-15 23:10:39 -0500

    add mustache template and wire up to server

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+var fs = require('fs');
 var express = require('express');
+var mustache = require('mustache');
+var TEMPLATE_PATH = './app/views/index.mustache';
+
 var app = express();
 
 app.get('/*', function(req, res) {
-  res.send('Up and running');
+  var template = fs.readFileSync(TEMPLATE_PATH, 'utf8');
+  var html = renderHtml(template, {title: 'Mustache Template', body: 'Hello World'});
+  res.send(html);
 });
 
 app.listen(8000, function() {
   console.log('app running at http://localhost:8000');
 });
+
+function renderHtml(template, data) {
+  return mustache.to_html(template, data);
+}
