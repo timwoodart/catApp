ed2fef2bfff9c68ec9ab047dd7c0c4bd4d8895d2
Timothy Wood <timwoodwood@gmail.com>
2016-02-15 23:18:21 -0500

    add request and get html from catApi

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 var fs = require('fs');
+var async = require('async');
 var express = require('express');
 var mustache = require('mustache');
+var _request = require('request');
 var TEMPLATE_PATH = './app/views/index.mustache';
 
 var app = express();
 
 app.get('/*', function(req, res) {
   var template = fs.readFileSync(TEMPLATE_PATH, 'utf8');
+
+  async.series({
+    cat: function(callback) {
+      _request('http://thecatapi.com/api/images/get?format=html', function(err, res, body) {
+        if (!err && res.statusCode == 200) {
+          callback(null, body);
+        } else {
+          callback(err);
+        }
+      })
+    }
+  }, function(err, results) {
+    console.log(results)
+  });
+
   var html = renderHtml(template, {title: 'Mustache Template', body: 'Hello World'});
   res.send(html);
 });
