1cfc88115ee4e2d09884e5ee1086561912acb07e
Timothy Wood <timwoodwood@gmail.com>
2016-02-16 00:24:16 -0500

    add test for api call

--- test/catApi-test.js
+++ test/catApi-test.js
@@ -1,5 +1,21 @@
-describe('Cat Api', function() {
-  it('should call catApi and get data returned', function() {
+var sinon = require('sinon');
+var assert = require('should');
+var proxyquire = require('proxyquire')
 
+describe('Cat Api', function() {
+  var catApi;
+  var stub = sinon.stub();
+
+  before(function(done) {
+    catApi = proxyquire('../app/services/catApi.js', { 'request': stub });
+    catApi.getCat = function(cb) { cb(null, '<catHtml/>') };
+    done();
+  });
+
+  it('should call catApi and get data returned', function(done) {
+    catApi.getCat( function(err, body) {
+      body.should.eql('<catHtml/>');
+      done();
+    });
   })
 });
